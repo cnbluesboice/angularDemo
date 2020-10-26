a9130c6a4a8f6086f4893ae23c09a90b44f14e6d
cnbluesboice <876430653@qq.com>
2020-10-26 11:48:10 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+// 根组件
+// 引入核心模块里的Component
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  selector: 'app-root', // 使用这个组件的名称
+  templateUrl: './app.component.html', // html
+  styleUrls: ['./app.component.less'] // css
 })
 export class AppComponent {
-  title = 'tsCliProgram';
+  title = 'tsCliProgram';  // 定义属性
 }
